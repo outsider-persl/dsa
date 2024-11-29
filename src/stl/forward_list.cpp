0385b20f68497a0afe832491967da9d0d1b6b352
outsider <outsider.persl@gmail.com>
2024-11-29 17:53:16 +0800

    config

--- src/stl/forward_list.cpp
+++ src/stl/forward_list.cpp
@@ -1,17 +1,15 @@
-#include <iostream>
 #include <forward_list>
+#include <iostream>
 
 int main() {
     std::forward_list<int> fl = {1, 2, 3, 4, 5};
 
     // 插入元素
     fl.push_front(0);  // 在前面插入
-
     // 遍历输出
     for (const auto& val : fl) {
         std::cout << val << " ";
     }
     std::cout << std::endl;
-
     return 0;
 }
